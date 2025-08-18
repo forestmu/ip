929ca4d204453933b87a67383828bb8fe7a7fb72
song lin <e1354455@u.nus.edu>
2025-08-18 09:43:17 +0800

    Level-2

--- src/main/java/Candy.java
+++ src/main/java/Candy.java
@@ -1,20 +1,28 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Candy {
+    private static ArrayList<String> allText = new ArrayList<>();
+
     public static void main(String[] args) {
         System.out.println("Hello! I am Candy🍬.");
         System.out.println("What can I do for you?\n");
 
         Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
-        System.out.println("    " + text);
         while (true) {
-            text = scanner.nextLine();
-            if (!text.equals("bye")) {
-                System.out.println("    " + text);
-            } else {
+            String text = scanner.nextLine();
+            if (text.equals("bye")) {
                 System.out.println("    Bye. Hope to see you again! ^-^");
                 break;
+            } else if (text.equals(("list"))) {
+                int max = allText.size();
+                for (int i = 0; i < max; i++) {
+                    int order = i + 1;
+                    System.out.println("    " + order + ". " + allText.get(i));
+                }
+            } else {
+                allText.add(text);
+                System.out.println("    " + text);
             }
         }
     }
