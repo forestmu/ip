7f9b0b258d744e53439e7f7df3cd62d534bd3d2f
song lin <e1354455@u.nus.edu>
2025-08-18 09:31:09 +0800

    Level-1

--- src/main/java/Candy.java
+++ src/main/java/Candy.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Candy {
     public static void main(String[] args) {
-        System.out.println("Hello! I am Candy.");
+        System.out.println("Hello! I am Candy🍬.");
         System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again!");
+
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        System.out.println("    " + text);
+        while (true) {
+            text = scanner.nextLine();
+            if (!text.equals("bye")) {
+                System.out.println("    " + text);
+            } else {
+                System.out.println("    Bye. Hope to see you again! ^-^");
+                break;
+            }
+        }
     }
 }
