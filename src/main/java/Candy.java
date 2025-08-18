97ee46e64047f9c22c6427e48bcfed6161e3c50d
song lin <e1354455@u.nus.edu>
2025-08-18 09:12:41 +0800

    Level-0

---
+++ src/main/java/Candy.java
@@ -0,0 +1,7 @@
+public class Candy {
+    public static void main(String[] args) {
+        System.out.println("Hello! I am Candy.");
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again!");
+    }
+}
