39f5482a086c4dd28f84c988b4b38be67a5e423b
song lin <e1354455@u.nus.edu>
2025-08-18 10:06:03 +0800

    Level-3

--- src/main/java/Candy.java
+++ src/main/java/Candy.java
@@ -2,26 +2,42 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Candy {
-    private static ArrayList<String> allText = new ArrayList<>();
+    private static ArrayList<Task> allText = new ArrayList<>();
 
     public static void main(String[] args) {
-        System.out.println("Hello! I am Candy🍬.");
-        System.out.println("What can I do for you?\n");
+        System.out.println("Hello! I am Candy🍬.\nWhat can I do for you?");
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String text = scanner.nextLine();
+            Task newTask = new Task(text);
             if (text.equals("bye")) {
                 System.out.println("    Bye. Hope to see you again! ^-^");
                 break;
             } else if (text.equals(("list"))) {
                 int max = allText.size();
+                System.out.println("    Here are the tasks in your list:");
                 for (int i = 0; i < max; i++) {
                     int order = i + 1;
-                    System.out.println("    " + order + ". " + allText.get(i));
+                    Task theTask = allText.get(i);
+                    System.out.println("    " + order + ". " + theTask.toString());
                 }
+            } else if (text.startsWith("mark ")) {
+                String number = text.substring(5);
+                int order = Integer.parseInt(number);
+                Task toMark = allText.get(order - 1);
+                toMark.markDone();
+                System.out.println("    Nice! I've marked this task as done: \n    "
+                        + toMark.toString());
+            } else if (text.startsWith("unmark ")) {
+                    String number = text.substring(7);
+                    int order = Integer.parseInt(number);
+                    Task toMark = allText.get(order - 1);
+                    toMark.markUndone();
+                    System.out.println("    Ok, I've marked this task as not done yet: \n    "
+                            + toMark.toString());
             } else {
-                allText.add(text);
+                allText.add(newTask);
                 System.out.println("    " + text);
             }
         }
