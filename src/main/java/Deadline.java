6650503d3c6224c5d6a90aeb9fedf2ccc4ae3496
song lin <e1354455@u.nus.edu>
2025-08-27 11:32:08 +0800

    print time in different format as input

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,19 @@
 public class Deadline extends Task {
 
-    protected String by;
+    protected Time by;
 
-    public Deadline(String description, boolean isDone, String by) {
+    public Deadline(String description, boolean isDone, Time by) {
         super(description, isDone);
         this.by = by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by:" + by + ")";
+        return "[D]" + super.toString() + " (by:" + by.toPrint() + ")";
     }
 
     @Override
     public String toSave() {
-        return "D | " + super.toSave() + " | " + by;
+        return "D | " + super.toSave() + " | " + by.toString();
     }
 }
