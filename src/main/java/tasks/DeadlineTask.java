07704db1a6b541867f4f606f86f9078058776338
song lin <e1354455@u.nus.edu>
2025-09-13 18:01:48 +0800

    Remove dead code

--- src/main/java/tasks/DeadlineTask.java
+++ src/main/java/tasks/DeadlineTask.java
@@ -19,18 +19,6 @@ public class DeadlineTask extends Task {
         this.by = information.getEndTime();
     }
 
-//    /**
-//     * Constructor of a deadline task
-//     *
-//     * @param description String description of the task
-//     * @param by  Time object of the due date
-//     * @param isDone  true if task is completed. False otherwise
-//     */
-//    public DeadlineTask(String description, boolean isDone, Time by) {
-//        super(description, isDone);
-//        this.by = by;
-//    }
-
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by:" + by.toPrint() + ")";
